2f94c0b709cedd6ff20ccf8869f71422cf3d7087
Brandon Washington <brandon@barbarians.com>
2022-03-01 20:35:32 +0530

    new commit

--- src/components/App.js
+++ src/components/App.js
@@ -3,8 +3,17 @@ import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import { AuthProvider } from "../contexts/AuthContext";
 import Chats from "./Chats";
 import Login from "./Login";
+import MobileComponent from "./MobileComponent";
 
 function App() {
+   const isMobile = window.matchMedia(
+    "only screen and (max-width: 760px)"
+  ).matches;
+
+  if (isMobile) {
+    // Display component onto screen or do whatever you want
+    return <MobileComponent />;
+  }
   return ( 
       <Router>
         <AuthProvider>
---
+++ src/components/MobileComponent.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const MobileComponent = () => {
+  return (
+    <div className="bgimg">
+        <div className="middle">
+            <h1>MOBILE VIEW COMING SOON</h1>
+            <hr/>
+            <p>On Progess</p>
+        </div>
+</div>
+  )
+}
+
+export default MobileComponent;
